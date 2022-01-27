012efe45326f8fc234a88b6f6462d99a99984657
evanykauh <94486303+evanykauh@users.noreply.github.com>
2022-01-26 20:25:27 -0800

    test first testfile successfully

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,12 +1,24 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.*;
+
 
 public class MarkdownParseTest {
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void testgetLinks() throws IOException{
+        List<String> expected = List.of("https://something.com", "some-page.html");
+        Path filename = Path.of("test-file.md");
+        String contents = Files.readString(filename);
+        ArrayList<String> actual =  MarkdownParse.getLinks(contents);
+        assertEquals(expected, actual);
     }
 }
 
 //javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
 //java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+//cmd shift P for classpath config 
+//set path to folder with junit and hamcrest testers
