53a7b401a8f44b40ae46b2894df50f403b4f6cb4
evanykauh <94486303+evanykauh@users.noreply.github.com>
2022-02-02 19:21:11 -0800

    update repo

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,37 +1,137 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
 import java.io.IOException;
+import java.util.List;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.*;
-
 
 public class MarkdownParseTest {
+    List<String> strList;
+
     @Test
-    public void testgetLinksFile1() throws IOException{
-        List<String> expected = List.of("https://something.com", "some-page.html");
-        Path filename = Path.of("test-file.md");
-        String contents = Files.readString(filename);
-        ArrayList<String> actual =  MarkdownParse.getLinks(contents);
-        assertEquals(expected, actual);
+    public void addition() {
+        assertEquals(2, 1 + 1);
     }
 
     @Test
-    public void testgetLinksmyFile2() throws IOException{
-        List<String> expected = List.of("https://www.youtube.com/watch?v=dQw4w9WgXcQ", 
-            "https://images-na.ssl-images-amazon.com/images/I/91MteSqsrJL.jpgage.html", 
-            "https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa752574(v=vs.85)?redirectedfrom=MSDN",
-            "https://www.adultswim.com/videos/rick-and-morty");
-        Path filename = Path.of("test-file-2.md");
-        String contents = Files.readString(filename);
-        ArrayList<String> actual =  MarkdownParse.getLinks(contents);
-        assertEquals("Expected array of links doesn't match the actual!", expected, actual);
+    public void testfile1() throws IOException {
+
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of("https://something.com", "some-page.html"));
     }
 
+    @Test
+    public void testfile2() throws IOException {
+
+        Path fileName = Path.of("test2-file.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of());
+    }
+    
+    @Test
+    public void testfile3() throws IOException {
+
+        Path fileName = Path.of("test3-file.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of("https://hello.com"));
+    }
+    
+    @Test
+    public void testfile4() throws IOException {
+
+        Path fileName = Path.of("test4-file.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of());
+    }
+    
+    @Test
+    public void testfile5() throws IOException {
+
+        Path fileName = Path.of("test-file2.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of("https://something.com", "some-page.html"));
+    }
+    
+    @Test
+    public void testfile6() throws IOException {
+
+        Path fileName = Path.of("test-file3.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of());
+    }
+    
+    @Test
+    public void testfile7() throws IOException {
+
+        Path fileName = Path.of("test-file4.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of());
+    }
+    
+    @Test
+    public void testfile8() throws IOException {
+
+        Path fileName = Path.of("test-file5.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of());
+    }
+        
+    @Test
+    public void testfile9() throws IOException {
+
+        Path fileName = Path.of("test-file6.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of("page.com"));
+    }
+        
+    @Test
+    public void testfile10() throws IOException {
+
+        Path fileName = Path.of("test-file7.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of());
+    }
+
+    @Test
+    public void testfile11() throws IOException {
+
+        Path fileName = Path.of("test-file8.md");
+        String contents = Files.readString(fileName);
+        this.strList = MarkdownParse.getLinks(contents);
+
+
+        assertEquals(this.strList, List.of());
+    }
 }
-
-//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
-//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
-//cmd shift P for classpath config 
-//set path to folder with junit and hamcrest testers
